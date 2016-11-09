09207f3819155b6b408c4c222982db99f2dedf6a
CMonkey <42.codemonkey@gmail.com>
2016-11-09 16:50:44 +0800

    add submit GroupKeyCheckPoint for shell

--- submit-job.sh
+++ submit-job.sh
@@ -21,3 +21,13 @@
     --supervise \
     ~/develop/spark-count/target/scala-2.11/spark-count_2.11-0.0.1.jar 
 
+### submit GroupKeyCheckPoint 
+~/gitrepo/spark/bin/spark-submit --class com.sparkcount.GroupKeyCheckPoint \
+    --master spark://10.204.43.43:7077 \
+    --deploy-mode cluster \
+    --executor-memory 5g \
+    --name wordcount \
+    --conf "spark.app.id=GroupKeyCheckPoint" \
+    --supervise \
+    ~/develop/spark-count/target/scala-2.11/spark-count_2.11-0.0.1.jar 
+
