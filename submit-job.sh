27520d42abed6ae78423d1d1a961bb854ec1a71b
CMonkey <42.codemonkey@gmail.com>
2016-11-19 10:08:26 +0800

    change submit job

--- submit-job.sh
+++ submit-job.sh
@@ -2,32 +2,32 @@
 
 ### submit wordcount 
 ~/gitrepo/spark/bin/spark-submit --class com.sparkcount.WordCount \
-    --master spark://10.204.43.43:7077 \
+    --master spark://10.0.0.5:7077 \
     --deploy-mode cluster \
     --executor-memory 5g \
     --name wordcount \
     --conf "spark.app.id=wordcount" \
     --supervise \
-    hdfs://archlinux.vagrant.vm:9000/usr/spark/jars/spark-count_2.11-0.0.1.jar \
-    hdfs:/archlinux.vagrant.vm:9000/usr/input/core-site.xml 20
+    hdfs://10.0.0.3:9000/usr/spark/jars/spark-count_2.11-0.0.1.jar \
+    hdfs:/10.0.0.3:9000/usr/input/core-site.xml 20
 
 ### submit TestGroupBy
 ~/gitrepo/spark/bin/spark-submit --class com.sparkcount.TestGroupBy \
-    --master spark://10.204.43.43:7077 \
+    --master spark://10.0.0.5:7077 \
     --deploy-mode cluster \
     --executor-memory 5g \
     --name wordcount \
     --conf "spark.app.id=groupby" \
     --supervise \
-    hdfs://archlinux.vagrant.vm:9000/usr/spark/jars/spark-count_2.11-0.0.1.jar \
+    hdfs://10.0.0.3:9000/usr/spark/jars/spark-count_2.11-0.0.1.jar \
 
 ### submit GroupKeyCheckPoint 
 ~/gitrepo/spark/bin/spark-submit --class com.sparkcount.GroupKeyCheckPoint \
-    --master spark://10.204.43.43:7077 \
+    --master spark://10.0.0.5:7077 \
     --deploy-mode cluster \
     --executor-memory 5g \
     --name wordcount \
     --conf "spark.app.id=GroupKeyCheckPoint" \
     --supervise \
-    hdfs://archlinux.vagrant.vm:9000/usr/spark/jars/spark-count_2.11-0.0.1.jar \
+    hdfs://10.0.0.3:9000/usr/spark/jars/spark-count_2.11-0.0.1.jar \
 
