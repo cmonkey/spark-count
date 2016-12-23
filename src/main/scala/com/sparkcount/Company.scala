3ffcdc97893445de932bc2dbde3843354c45273a
CMonkey <42.codemonkey@gmail.com>
2016-12-23 09:50:15 +0800

    change Company

--- src/main/scala/com/sparkcount/Company.scala
+++ src/main/scala/com/sparkcount/Company.scala
@@ -1,3 +1,3 @@
 case class Company(name: String, children: Seq[Company]=Nil)
 
-val companies = List(Company("B"), Company("A"), Company("T"))
+/*val companies = List(Company("B"), Company("A"), Company("T"))*/
