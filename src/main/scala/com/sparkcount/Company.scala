b13f5c75583720bbb72fa127cd4bfa15b1b7e988
CMonkey <42.codemonkey@gmail.com>
2016-12-23 09:57:17 +0800

    refator Company

--- src/main/scala/com/sparkcount/Company.scala
+++ src/main/scala/com/sparkcount/Company.scala
@@ -1,3 +1,13 @@
 case class Company(name: String, children: Seq[Company]=Nil)
 
 /*val companies = List(Company("B"), Company("A"), Company("T"))*/
+
+object CompanyObj{
+
+  def main(args: Array[String]): Unit = {
+
+    val companies = List(Company("B"), Company("A"), Company("T"))
+
+    println(companies)
+  }
+}
