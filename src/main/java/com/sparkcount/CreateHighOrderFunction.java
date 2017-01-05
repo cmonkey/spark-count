f28ecdc15e0544f4e02b16517f41c9ff298b7626
CMonkey <42.codemonkey@gmail.com>
2017-01-05 09:40:03 +0800

    add CreateHighOrderFunction

---
+++ src/main/java/com/sparkcount/CreateHighOrderFunction.java
@@ -0,0 +1,42 @@
+package com.sparkcount;
+
+import com.clearspring.analytics.util.Lists;
+import scala.Int;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Created by cmonkey on 1/5/17.
+ */
+public class CreateHighOrderFunction {
+
+    private static List<Integer> list;
+
+    private static List<Integer> result = Lists.newArrayList();
+
+    public CreateHighOrderFunction(List<Integer> list){
+        CreateHighOrderFunction.list = list;
+    }
+
+    // find Even Number is a function which takes a Prodicate
+
+    public static List findEvenNumbers(Predicate predicateToApply){
+
+        BiFunction<List, Predicate, List> filter;
+
+        filter = (listofIntegers, predicate) -> {
+            //result.addAll(list.stream().filter(predicate).collect(Collectors.toList()));
+            result.addAll((Collection<? extends Integer>) list.stream().filter(predicate).collect(Collectors.toList()));
+            return result;
+        };
+
+        filter.apply(list, predicateToApply);
+
+        return result;
+    }
+}
