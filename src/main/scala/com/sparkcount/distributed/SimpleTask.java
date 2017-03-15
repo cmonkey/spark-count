db80922fc10d76070b2869de053d7ae267d83769
CMonkey <42.codemonkey@gmail.com>
2017-03-15 20:47:37 +0800

    change SimpleTask

--- src/main/scala/com/sparkcount/distributed/SimpleTask.java
+++ src/main/scala/com/sparkcount/distributed/SimpleTask.java
@@ -1,13 +1,5 @@
 package com.sparkcount.distributed;
 
-import java.io.Serializable;
-
-abstract class Task implements Serializable{
-    public void run(){
-        System.out.println("run task");
-    }
-}
-
 public class SimpleTask extends Task{
     @Override
     public void run(){
