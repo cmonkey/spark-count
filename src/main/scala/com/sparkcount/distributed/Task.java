e5c9674a09b156d176282c45fc470bea2f07b826
CMonkey <42.codemonkey@gmail.com>
2017-03-15 20:47:18 +0800

    add Task

---
+++ src/main/scala/com/sparkcount/distributed/Task.java
@@ -0,0 +1,14 @@
+package com.sparkcount.distributed;
+
+import java.io.Serializable;
+
+/**
+ * Created by cmonkey on 3/15/17.
+ */
+public abstract class Task implements Serializable{
+
+    public void run(){
+
+        System.out.println("run task!");
+    }
+}
