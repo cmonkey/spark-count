5be3b6606a0836a842c9bf241a8827a3c868fb0d
CMonkey <42.codemonkey@gmail.com>
2017-03-15 21:01:11 +0800

    change TestSerializer

--- src/main/scala/com/sparkcount/distributed/TestSerializer.scala
+++ src/main/scala/com/sparkcount/distributed/TestSerializer.scala
@@ -6,8 +6,11 @@ object TestSerializer{
 
     val task = new SimpleTask()
     FileSerializer.writeObjectToFile(task, "task.ser")
+    ClassManipulator.saveClassFile(task)
 
+    /*
     val readObjectTask = FileSerializer.readObjectFromFile("task.ser").asInstanceOf[Task]
     readObjectTask.run()
+    */
   }
 }
