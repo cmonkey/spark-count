d6bf813555b2957d6bac6a2f3b7f5b98c55275e8
CMonkey <42.codemonkey@gmail.com>
2017-03-15 21:01:23 +0800

    add ClassManipulator

---
+++ src/main/scala/com/sparkcount/distributed/ClassManipulator.scala
@@ -0,0 +1,27 @@
+package com.sparkcount.distributed
+
+import java.io.FileOutputStream
+
+object ClassManipulator{
+  def saveClassFile(obj: AnyRef): Unit = {
+    val classLoader = obj.getClass.getClassLoader
+
+    val className = obj.getClass.getName
+
+    val classFile = className.replace('.', '/')+".class"
+    val stream = classLoader.getResourceAsStream(classFile)
+
+    // just use the class simple name as the file name 
+
+    val outputFile = className.split(',').last + ".class"
+    val fileStream = new FileOutputStream(outputFile)
+    var data = stream.read()
+    while(data != -1){
+      fileStream.write(data)
+      data = stream.read()
+    }
+
+    fileStream.flush()
+    fileStream.close()
+  }
+}
