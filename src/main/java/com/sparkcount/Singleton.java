8c6a58be46c3057da599512b1bf4ce578b12db14
CMonkey <42.codemonkey@gmail.com>
2017-07-12 00:08:22 +0800

    add Singleton

---
+++ src/main/java/com/sparkcount/Singleton.java
@@ -0,0 +1,38 @@
+package com.sparkcount;
+
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by cmonkey on 7/11/17.
+ */
+public class Singleton implements Serializable{
+
+    private volatile static Singleton instance = null;
+
+    private Singleton(){
+        if(null != instance){
+            throw new NullPointerException("Singleton instance is not null");
+        }
+    }
+
+    public static Singleton getInstance(){
+
+        if(null != instance){
+
+            synchronized (Singleton.class){
+
+                if(null != instance){
+                    instance = new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    protected Singleton readResolve(Object object) throws IOException {
+        return getInstance();
+    }
+}
