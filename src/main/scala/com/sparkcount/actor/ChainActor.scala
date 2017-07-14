a05a3a9be4fe9a040514230fd176408c5ce87a65
CMonkey <42.codemonkey@gmail.com>
2017-07-14 13:34:34 +0800

    add ChainActor

---
+++ src/main/scala/com/sparkcount/actor/ChainActor.scala
@@ -0,0 +1,24 @@
+import akka.actor.{Actor, ActorRef}
+
+import Actor._
+
+
+class ChainActor extends Actor {
+
+  def receive = {
+    case "s" => println("Hello s")
+  }
+  def test(): Unit = {
+    println("Hello Actor")
+  }
+}
+
+object TestActor{
+
+  def main(args: Array[String]):Unit = {
+    val chainActor = new ChainActor
+
+    chainActor.preStart()
+  }
+}
+
