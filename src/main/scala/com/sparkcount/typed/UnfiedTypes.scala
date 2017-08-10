8c66e9d51cf164fe17711156e2a5edd0e57b8983
CMonkey <42.codemonkey@gmail.com>
2017-08-10 14:24:12 +0800

    add typed

---
+++ src/main/scala/com/sparkcount/typed/UnfiedTypes.scala
@@ -0,0 +1,23 @@
+package com.sparkcount.typed
+
+object UnfiedTypes{
+
+  def main(args: Array[String]): Unit = {
+    val list: List[Any] = List(
+      "a string",
+      732,
+      'c',
+      true,
+      () => "an anonymous function returning a string"
+    )
+    list.foreach(element => println(element))
+
+    val x: Long = 987654321
+    val y: Float = x
+
+    val face: Char = '@'
+    val number: Int = face
+
+    println(number)
+  }
+}
