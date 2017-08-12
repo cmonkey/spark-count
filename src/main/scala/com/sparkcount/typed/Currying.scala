c3cf6991628b076e8d9cceabc19bf279e511d34b
CMonkey <42.codemonkey@gmail.com>
2017-08-12 18:09:58 +0800

    add Currying

---
+++ src/main/scala/com/sparkcount/typed/Currying.scala
@@ -0,0 +1,24 @@
+package com.sparkcount.typed
+
+object Curry{
+
+  def filter(xs: List[Int], p: Int => Boolean): List[Int] = {
+    if (xs.isEmpty){
+      xs
+    } else if (p(xs.head)){
+      xs.head :: filter(xs.tail, p)
+    }else{
+      filter(xs.tail, p)
+    }
+  }
+
+  def modN(n: Int)(x: Int) = {
+    (x % n) == 0
+  }
+
+  def main(args:Array[String]): Unit = {
+    val nums = List(1,2,3,4,5,6,7,8)
+    println(filter(nums, modN(2)))
+    println(filter(nums, modN(3)))
+  }
+}
