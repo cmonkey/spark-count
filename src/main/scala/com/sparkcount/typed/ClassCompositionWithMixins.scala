83d02109f314b29c1887081a2718ba7b729133b2
CMonkey <42.codemonkey@gmail.com>
2017-08-12 17:58:40 +0800

    add RichIterator

--- src/main/scala/com/sparkcount/typed/ClassCompositionWithMixins.scala
+++ src/main/scala/com/sparkcount/typed/ClassCompositionWithMixins.scala
@@ -14,6 +14,30 @@ trait C extends A {
 
 class D extends B with C 
 
+abstract class AbsIterator{
+  type T
+  def hasNext: Boolean
+  def next(): T
+}
+
+class StringIterator(s: String) extends AbsIterator{
+  type T = Char
+  private var i = 0
+  def hasNext = i < s.length
+  def next() = {
+    val ch = s charAt i 
+    i += 1
+    ch
+  }
+}
+
+trait RichIterator extends AbsIterator{
+  def foreach(f: T => Unit): Unit = 
+    while (hasNext)
+      f(next())
+}
+
+
 object ClassCompositionWithMixins{
 
   def main(args: Array[String]): Unit = {
@@ -21,5 +45,9 @@ object ClassCompositionWithMixins{
 
     println(s"d.message = $d.message")
     println(s"d.loudMessage = $d.loudMessage")
+
+    class Iter extends StringIterator(args(0)) with RichIterator 
+    val iter = new Iter
+    iter foreach println
   }
 }
