e1e42ca9b892ebdf34a6bf489451c633bb1d8222
CMonkey <42.codemonkey@gmail.com>
2017-08-23 01:49:01 +0800

    add WorkProducer

--- src/main/scala/com/sparkcount/timeWheel/WorkProducer.scala
+++ src/main/scala/com/sparkcount/timeWheel/WorkProducer.scala
@@ -1,5 +1,13 @@
 package com.sparkcount.timeWheel
 
-class WorkProducer {
+class WorkProducer(val pointor: Int, val slot: Slot[AbstractTask]) extends Runnable{
+  override def run() = {
 
+    val task = slot.getTask(pointor)
+
+    task match {
+      case Some(x) => x.run()
+      case None => println("error task ")
+    }
+  }
 }
