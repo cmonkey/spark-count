611dea848643b8b9744c22de2bd9eddc95bce21f
CMonkey <42.codemonkey@gmail.com>
2017-09-05 17:32:50 +0800

    add ImplicitClass

---
+++ src/main/scala/com/sparkcount/typed/ImplicitClass.scala
@@ -0,0 +1,25 @@
+package com.sparkcount.typed
+
+object Helpers {
+
+  implicit class IntWithTimes(x: Int){
+    def times[A](f: => A): Unit = {
+      def loop(current: Int): Unit = {
+        if (current > 0){
+          f
+          loop(current - 1)
+        }
+      }
+
+      loop(x)
+    }
+  }
+
+}
+
+object ImplicitClassApp extends App{
+
+  import Helpers._
+
+  5 times println("Hi")
+}
