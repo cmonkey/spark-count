3439b5f545e605b3fa9a4bd93c860230d8468b16
CMonkey <42.codemonkey@gmail.com>
2017-10-09 14:30:32 +0800

    add TestJunit5

---
+++ src/test/java/com/sparkcount/test/TestJunit5.java
@@ -0,0 +1,46 @@
+package com.sparkcount.test;
+
+import org.junit.jupiter.api.*;
+
+public class TestJunit5 {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("beforeAll executed");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("beforeEach executed");
+    }
+
+    @Tag("dev")
+    @Test
+    void testDevCalc(){
+
+        System.out.println("test dev executed");
+        Assertions.assertEquals(4, add(2, 2));
+    }
+
+    @Tag("prod")
+    @Disabled
+    @Test
+    void testProdCalc(){
+        System.out.println("prod dev executed");
+        Assertions.assertEquals(4, add(2, 2));
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After All executed");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("after each executed");
+    }
+
+    private int add(int x, int y) {
+        return x + y;
+    }
+}
