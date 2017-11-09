bdb58c8dbb37ee21e925928f2878a16e6a401ec9
CMonkey <42.codemonkey@gmail.com>
2017-11-09 10:30:24 +0800

    add java/org/github/cmonkey/spark

---
+++ src/main/java/org/github/cmonkey/spark/JMMOut.java
@@ -0,0 +1,41 @@
+package org.github.cmonkey.spark;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by cmonkey on 17-7-12.
+ */
+public class JMMOut {
+
+    public static void main(String[] args){
+        JMMOut jmmOut = new JMMOut();
+
+        jmmOut.call(jmmOut);
+    }
+
+    public void call(JMMOut jmmOut){
+
+        new MyTask(jmmOut).start();
+    }
+
+    class MyTask extends Thread{
+
+        private WeakReference<JMMOut> reference;
+
+        public MyTask(JMMOut jmmOut){
+            reference = new WeakReference<JMMOut>(jmmOut);
+        }
+
+        private JMMOut doSome(){
+
+            return new JMMOut();
+        }
+        @Override
+        public void run(){
+
+            if(reference.get() != null) {
+                doSome();
+            }
+        }
+    }
+}
