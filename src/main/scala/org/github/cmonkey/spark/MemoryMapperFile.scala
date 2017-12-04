77da506d1a4c243c4d1b7832eed3c2940042a2df
CMonkey <42.codemonkey@gmail.com>
2017-12-04 11:05:01 +0800

    add MemoryMapperFile

--- src/main/scala/org/github/cmonkey/spark/MemoryMapperFile.scala
+++ src/main/scala/org/github/cmonkey/spark/MemoryMapperFile.scala
@@ -2,17 +2,36 @@ package org.github.cmonkey.spark
 
 import java.io.RandomAccessFile
 import java.nio.channels.FileChannel
+import java.util
 
 object MemoryMapperFile extends App{
 
+  /*
   val count = 10285760
 
-  val fileName = ""
+  val fileName = "/home/cmonkey/.profile"
 
-  val memoryMapperFile = new RandomAccessFile(fileName, "rw")
+  val memoryMapperFile = new RandomAccessFile(fileName, "rw").getChannel
 
-  val out = memoryMapperFile.getChannel.map(FileChannel.MapMode.READ_WRITE, 0, count)
+  val out = memoryMapperFile.map(FileChannel.MapMode.READ_WRITE, 0, memoryMapperFile.size())
+
+  System.out.println(out.isLoaded)
+  System.out.println(out.capacity())
+
 
   memoryMapperFile.close()
+  */
 
+  val startTime = System.currentTimeMillis()
+
+  val count = 10000000
+  val map = new util.HashMap[String, String](count) {}
+
+  for(i <- 0 until count){
+    map.put("hi"+i, "v"+i)
+  }
+
+  val endTime = System.currentTimeMillis()
+
+  System.out.println("time = " + (endTime - startTime))
 }
