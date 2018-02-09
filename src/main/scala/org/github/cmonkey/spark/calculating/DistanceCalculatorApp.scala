42fedc01f3e20d403e47ec0d1d89774616181103
CMonkey <42.codemonkey@gmail.com>
2018-02-09 10:25:04 +0800

    change DistanceCalculatorApp

--- src/main/scala/org/github/cmonkey/spark/calculating/DistanceCalculatorApp.scala
+++ src/main/scala/org/github/cmonkey/spark/calculating/DistanceCalculatorApp.scala
@@ -3,7 +3,7 @@ package org.github.cmonkey.spark.calculating
 object DistanceCalculatorApp extends App{
 
   val r = new DistanceCalculatorImpl().calulateDistanceInKilomenter(Location(10, 20),
-    Location(40,20))
+    Location(40,30))
 
   println(s"$r")
 
