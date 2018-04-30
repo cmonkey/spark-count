8b4a458def08fcf7260d5240b72497d041315cf6
CMonkey <42.codemonkey@gmail.com>
2018-04-30 19:07:48 +0800

    add TestUnsafe

---
+++ src/main/java/org/github/cmonkey/spark/TestUnsafe.java
@@ -0,0 +1,47 @@
+package org.github.cmonkey.spark;
+
+import sun.misc.Unsafe;
+
+public class TestUnsafe{
+
+    public static void main(String[] args) {
+        System.err.close();
+        int count = 0;
+        while (count++ < 120000) {
+            test();
+        }
+    }
+
+
+
+    public static void test() {
+        byte[] newBuf = serBytes(397);
+        if (newBuf.length != 397) {
+            System.out.println("ERROR_MSG");
+        }
+    }
+
+    static final long BYTE_ARRAY_BASE_OFFSET = (long)getUnsafe().arrayBaseOffset(byte[].class);
+
+    public static byte[] serBytes(int bufLen) {
+        byte[] buf = new byte[bufLen];
+        getUnsafe().putInt(buf, BYTE_ARRAY_BASE_OFFSET + 1L, buf.length);
+        System.err.println("ref " + buf);
+        return buf;
+    }
+
+	public static Unsafe getUnsafe() {
+		try {
+			java.lang.reflect.Field singletonInstanceField = Unsafe.class.getDeclaredField("theUnsafe");
+			singletonInstanceField.setAccessible(true);
+			return (Unsafe) singletonInstanceField.get(null);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+}
