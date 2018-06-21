e7415b7ba8ad34f5138c5aad5040ea7c0ef4cc07
CMonkey <42.codemonkey@gmail.com>
2018-06-21 10:52:31 +0800

    add RingBuffer

---
+++ src/main/scala/org/github/cmonkey/spark/RingBuffer.scala
@@ -0,0 +1,47 @@
+package org.github.cmonkey.spark
+
+class RingBuffer[T: manifest](capacity: Int) {
+
+  private var _head = 0
+  private var _tail = 0
+  private var _size = 0
+
+  private val queue = new Array[T](capacity)
+
+  def size:Int = this._size
+
+  def isEmpty: Boolean = this._size == 0
+
+  def isFull: Boolean = this._size == capacity
+
+  def peek:T = this.queue(this._head)
+
+  def enqueue(elem: T): Int = {
+    this.queue(this._tail) = elem 
+
+    if(!this.isFull) {
+      this._size += 1
+    }else{
+      this._head = (this._head + 1) % this.capacity
+    }
+
+    this._tail = (this._head + this._size) % this.capacity
+
+    this._size
+  }
+
+  def dequeue:T = {
+    if(this.isEmpty){
+      throw new Exception("RingBuffer is empty")
+    }
+
+    val elem = this.peek
+
+    this._size -= 1
+
+    this._head = (this._head + 1) % this.capacity
+
+    elem
+  }
+
+}
