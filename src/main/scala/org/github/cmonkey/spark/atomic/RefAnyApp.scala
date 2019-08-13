846d5b3921446291c494a974bb740017509e0330
CMonkey <42.codemonkey@gmail.com>
2019-08-13 15:08:18 +0800

    add test transformAndGet to RefAnyApp

--- src/main/scala/org/github/cmonkey/spark/atomic/RefAnyApp.scala
+++ src/main/scala/org/github/cmonkey/spark/atomic/RefAnyApp.scala
@@ -2,4 +2,7 @@ package org.github.cmonkey.spark.atomic
 
 object RefAnyApp extends App{
 
+  val ref = new RefAny(2)
+  val transformAndGet = ref.transformAndGet(x => x + 2)
+  println(s"transformAndGet = ${transformAndGet}")
 }
