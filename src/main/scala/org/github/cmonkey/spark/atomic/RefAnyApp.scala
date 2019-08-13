ef84b42aa7ea8d46f57bbcf366a1c179741064a9
CMonkey <42.codemonkey@gmail.com>
2019-08-13 15:12:37 +0800

    add test transformAndGetQueue to RefAnyApp

--- src/main/scala/org/github/cmonkey/spark/atomic/RefAnyApp.scala
+++ src/main/scala/org/github/cmonkey/spark/atomic/RefAnyApp.scala
@@ -1,8 +1,16 @@
 package org.github.cmonkey.spark.atomic
 
+import scala.collection.immutable.Queue
+
 object RefAnyApp extends App{
 
   val ref = new RefAny(2)
   val transformAndGet = ref.transformAndGet(x => x + 2)
   println(s"transformAndGet = ${transformAndGet}")
+
+  val refQueue = new RefAny(Queue.empty[String])
+
+  val transformAndGetQueue =  refQueue.transformAndGet(q => q.enqueue("Alex"))
+
+  println(s"transformAndGetQueue = ${transformAndGetQueue}")
 }
