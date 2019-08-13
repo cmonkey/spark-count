f93419b98f441e759a71ecc9348079887f4d644d
CMonkey <42.codemonkey@gmail.com>
2019-08-13 15:02:44 +0800

    add override Ref to RefAny

--- src/main/scala/org/github/cmonkey/spark/atomic/RefAny.scala
+++ src/main/scala/org/github/cmonkey/spark/atomic/RefAny.scala
@@ -1,5 +1,16 @@
 package org.github.cmonkey.spark.atomic
 
-class RefAny {
+import java.util.concurrent.atomic.AtomicReference
 
+class RefAny[T](initial: T) extends Ref[T]{
+
+  private[this] val instance = new AtomicReference(initial)
+
+  override def get: T = instance.get()
+
+  override def set(update: T): Unit = instance.set(update)
+
+  override def compareAndSet(expect: T, update: T): Boolean = instance.compareAndSet(expect, update)
+
+  override def transformAndGet(cb: T => T): T = ???
 }
