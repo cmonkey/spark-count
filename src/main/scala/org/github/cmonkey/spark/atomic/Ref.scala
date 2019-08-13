e3c6ad7dc009c412603c94f01b2a9cdda5e5ba33
CMonkey <42.codemonkey@gmail.com>
2019-08-13 15:02:30 +0800

    add Ref.{get set compareAndSet transformAndGet}

--- src/main/scala/org/github/cmonkey/spark/atomic/Ref.scala
+++ src/main/scala/org/github/cmonkey/spark/atomic/Ref.scala
@@ -1,5 +1,13 @@
 package org.github.cmonkey.spark.atomic
 
-class Ref {
+trait Ref[T] {
+  def get: T
+  def set(update: T)
+  def compareAndSet(expect: T, update: T): Boolean
 
+  def transformAndGet(cb: T => T): T
+}
+
+object Ref{
+  def apply[T](initial: T) = new RefAny(initial)
 }
