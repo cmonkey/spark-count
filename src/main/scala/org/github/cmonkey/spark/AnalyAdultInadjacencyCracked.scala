5b5c91b7960f1b6be98283bf0353a7e84842a794
CMonkey <42.codemonkey@gmail.com>
2019-10-15 11:15:41 +0800

    add read firefox-adult-inadjacency-cracked file to AnalyAdultInadjacencyCracked

--- src/main/scala/org/github/cmonkey/spark/AnalyAdultInadjacencyCracked.scala
+++ src/main/scala/org/github/cmonkey/spark/AnalyAdultInadjacencyCracked.scala
@@ -1,5 +1,18 @@
 package org.github.cmonkey.spark
 
-class AnalyAdultInadjacencyCracked {
+import org.apache.spark.sql.SparkSession
 
+object AnalyAdultInadjacencyCracked {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("AnalyAdultInadjacencyCracked")
+      .master("local[*]")
+      .getOrCreate()
+
+    val file = "resources/firefox-adult-inadjacency-cracked.txt"
+
+    val lines = spark.read.textFile(file)
+
+    lines.show(100)
+  }
 }
