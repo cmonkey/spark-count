576e3e1300791849555bd0805ca655393f242666
CMonkey <42.codemonkey@gmail.com>
2019-11-29 22:27:37 +0800

    add MethodChainHints

---
+++ src/main/scala/org/github/cmonkey/spark/MethodChainHints.scala
@@ -0,0 +1,14 @@
+package org.github.cmonkey.spark
+
+object MethodChainHints{
+  val words = Seq("a", "sequence", "of", "words")
+
+  words
+  .vide
+  .map(_.length)
+  .filter(_ > 3)
+  .toSeq
+  .distinct
+  .headOption
+  .getOrElse(0)
+}
